13d97aa2c6551f7a2b6cce67df2e1e26dfc1b681
captainstevos <steven.kuo18@gmail.com>
2014-06-08 14:14:55 -0400

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,9 +1,10 @@
+##Read in the data
 setwd("C:/Users/558966/Documents/Coursera")
 data <- read.table("household_power_consumption.txt", header=T, sep=";", na.strings="?", colClasses = c("factor", "factor","numeric", "numeric", "numeric", "numeric","numeric","numeric","numeric"))
-
+## Format the dates and subset to specified timeframe
 data$date.new <- as.Date(data$Date, format ="%d/%m/%Y")
 data.new <- subset(data, data$date.new >= '2007-02-01' & data$date.new <= '2007-02-02')
-
+##Create the histogram and copy as a png
 hist(data.new$Global_active_power, col = "red",xlab = "Global Active Power (kilowatts)",main = "Global Active Power", ylim = c(0,1200))
 dev.copy(png,'plot1.png', width = 480, height = 480)
 dev.off()
